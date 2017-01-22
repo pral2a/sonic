aec2cd55529132206222e05ec7fa9527f4d10d11
Pral2a <guillem@pral2a.com>
2017-01-22 23:17:03 +0100

    Update sonics.sh

--- sonics.sh
+++ sonics.sh
@@ -4,13 +4,12 @@
 # ex: $ sonics originals "echo 0.5 0.5 0.5 0.5"
 
 
-EXPORT_FOLDER=$(echo $2 | tr ' ' '_' | tr '.' '_'  | tr '-' '_')
+EXPORT_FOLDER=$(echo $2 | tr ',' '_' | tr ' ' '_' | tr '.' '_'  | tr '-' '_')
 INPUT_FOLDER=$1
 FX=$2
-DOVIDEO=$4
-DOWNSAMPLE=${3-false}
+DOVIDEO=$3
 
-parallel sonicnew ::: ls $INPUT_FOLDER/*.tiff ::: $EXPORT_FOLDER ::: "$FX" ::: $DOWNSAMPLE
+parallel sonicnew ::: ls $INPUT_FOLDER/*.tiff ::: $EXPORT_FOLDER ::: "$FX"
 
 if [ "$DOVIDEO" == "video" ]; then
 	
@@ -45,4 +44,3 @@ echo "Done in $EXPORT_FOLDER"
 if [ "$(uname)" == "Darwin" ]; then
 	osascript -e 'display notification "Process done!" with title "Sonics"'     
 fi
-
