bdc2b668ea2e16d381d466bd605a23a8c2c44900
Pral2a <guillem@pral2a.com>
2017-01-22 23:16:38 +0100

    Update sonicnew.sh

--- sonicnew.sh
+++ sonicnew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# $ source.jpg export_folder "effects" sampling
-# ex: $  img.jpg exports "echo 0.5 0.5 0.5 0.5" down
+# $ source.jpg export_folder "effects”
+# ex: $  img.jpg exports "echo 0.5 0.5 0.5 0.5”
 
 if [[ -d "$1" ]]; then
     cd "$1"
@@ -14,21 +14,15 @@ fi
 # INTERNALS
 SIZE=$(identify -ping -format '%wx%h' $FILEPATH 2>&1)
 DEPTH=$(identify -ping -format '%z' $FILEPATH 2>&1)
-PROCESSDEPTH=$DEPTH
-EXPORT_FOLDER=$(dirname $(dirname "$FILEPATH"))'/'$2 #This is super ugly
+PROCESSDEPTH=8
+EXPORT_FOLDER=$(dirname $(dirname "$FILEPATH"))'/'$2 #This is super ugly and needs to be changed
 FILENAME=$(basename "$FILEPATH")
 EXTENSION="${FILENAME##*.}"
 FILENAME="${FILENAME%.*}"
 
 # FX paramas
-ENCODING="unsigned-integer" #u-law
+ENCODING="u-law" #unsigned-integer OR u-law
 FX=${3-echo 1 1 1 1}
-DOWNSAMPLE=$4
-
-# SET downsample effect
-if [ "$DOWNSAMPLE" == "down" ]; then
-	PROCESSDEPTH=8
-fi
 
 # CHECK IF FOLDER
 if [ ! -d $EXPORT_FOLDER ]; then
@@ -36,9 +30,17 @@ if [ ! -d $EXPORT_FOLDER ]; then
   mkdir -p $EXPORT_FOLDER;
 fi
 
+echo "$DEPTH"
+
+if [ "$DEPTH" -gt "8" ]; then
+   echo "Converting $FILENAME to 8 bits ..."
+   mogrify -depth 8 -format $EXTENSION $FILENAME.$EXTENSION
+fi
+
+
 echo "Processing ... $FILENAME with $FX"
 
 # MAGIC
-convert $EXTENSION:$FILEPATH rgb:- | sox --type raw --bits $PROCESSDEPTH --encoding $ENCODING --channels 1 --rate 44.1k - --type raw - $FX | convert -depth $DEPTH -size $SIZE rgb:- $EXTENSION:$EXPORT_FOLDER/$FILENAME.tiff
+convert $EXTENSION:$FILEPATH rgb:- | sox --type raw --bits $PROCESSDEPTH --encoding $ENCODING --channels 1 --rate 44.1k - --type raw - $FX | convert -depth $DEPTH -size $SIZE rgb:- $EXTENSION:$EXPORT_FOLDER/$FILENAME.$EXTENSION
 
 echo "Output result saved on $EXPORT_FOLDER/$FILENAME.tiff"
